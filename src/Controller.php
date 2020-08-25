719ba58f02c38b23b05581b682292e14b060366d
jybtx <2574034626@qq.com>
2020-08-25 18:00:07 +0800

    first commit

---
+++ src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Jybtx\WangEditor;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{}
---
+++ src/WangEditorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php 
+namespace Jybtx\WangEditor;
+
+use Illuminate\Support\ServiceProvider;
+
+class WangEditorServiceProvider extends ServiceProvider
+{
+	
+	/**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
